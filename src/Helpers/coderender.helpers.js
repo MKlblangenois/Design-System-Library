3f1842f9057c88a467e3eb1cfd07e2ce1a7ff651
Logan <logan.blangenois@mediakod.com>
2019-12-31 15:08:21 +0100

    set spacing element

--- src/Helpers/coderender.helpers.js
+++ src/Helpers/coderender.helpers.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from "styled-components";
+import Highlight, {defaultProps} from 'prism-react-renderer'
 
 
 const StyledCodeRender = styled.code`
@@ -10,6 +11,10 @@ const StyledCodeRender = styled.code`
     border: 1px solid #EEEEEE;
     background-color: #F8F8F8;
     border-radius: 3px;
+    * {
+        font-family: "Operator Mono","Fira Code Retina","Fira Code","FiraCode-Retina","Andale Mono","Lucida Console",Consolas,Monaco,monospace!important;
+        font-size: 14px!important;
+    }
     span { display: block }
 `
 
@@ -21,6 +26,20 @@ const CodeRender = ({children}) => {
         <StyledCodeRender>
             <pre>{children}</pre>
         </StyledCodeRender>
+
+        // <Highlight {...defaultProps} code={children} language='javascript'>
+        //     {({className, style, tokens, getLineProps, getTokenProps}) => (
+        //     <pre className={className} style={{...style, padding: '20px'}}>
+        //         {tokens.map((line, i) => (
+        //         <div key={i} {...getLineProps({line, key: i})}>
+        //             {line.map((token, key) => (
+        //             <span key={key} {...getTokenProps({token, key})} />
+        //             ))}
+        //         </div>
+        //         ))}
+        //     </pre>
+        //     )}
+        // </Highlight>
     )
 }
 
